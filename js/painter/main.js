063195a20960c638b7465c15c6b6b29b07f104a9
irmalien <mikus.vanags@gmail.com>
2019-01-10 01:26:01 +0100

    cleanup

--- js/painter/main.js
+++ js/painter/main.js
@@ -1,18 +1,27 @@
-const windowWidth = document.documentElement.clientWidth-17;
-const windowHeight = document.documentElement.clientHeight-17;
+const windowWidth = document.documentElement.clientWidth;
+const windowHeight = document.documentElement.clientHeight;
 
 const canvasW = windowWidth  //5606 //1000 //4400; //windowWidth //2200
 const canvasH = windowHeight   //3737 //667 //2616; //windowHeight //1308
 
 let count = 0;
-let bubbleArray = [];
-let bubbleQuantity = 10;
+let atomArray = [];
+let atomQuantity = 10;
 
-const bubble = {
+// const atomQuantity2 ={
+//   value: 10,
+
+//   set( newValue, para ) { return; this.value = newValue },
+//   get()           { return this.value; }
+// },
+
+const atom = {
 
   //OBJECT
-  minSize: 0.5,
-  maxSize: 10,
+  sizeMin: 0.5,
+  sizeMax: 10,
+  sizeRand: 0.05,
+
   // roundness: 1,
 
   // minStroke: 1, 
@@ -28,27 +37,27 @@ const bubble = {
   alphaMin: 0.9,
   alphaMax: 1.0,
 
-  //MOVEMENT - 1 is normal , less is faster, more is slower
-  //DYNAMICS
-  //vertical //gravity
-  //horisontal //wind
-  //speed
-  //Jitter
-  //
+  //DYNAMICS (relative to atom radius)
 
-  movement: 3,
+  movVer: 0,
+  movHor: 0,
+  movTremble: 1,
+  movSpeed: 0.75,
+  movRandX: 0.005,
+  movRandY: 0.005,
+  movRandZ: 0.5,
 };
 
 let palettesData; //imported json data
 let palette;
 let paletteArray = [];
 
-function relativeSize(){
-  const relativeSize = canvasW/1000;
-  bubble.minSize = bubble.minSize * relativeSize;
-  bubble.maxSize = bubble.maxSize * relativeSize;
-  bubble.movement = bubble.movement * relativeSize;
-}
+// function relativeSize(){
+//   const relativeSize = canvasW/1000;
+//   atom.minSize = atom.minSize * relativeSize;
+//   atom.maxSize = atom.maxSize * relativeSize;
+//   atom.movement = atom.movement * relativeSize;
+// }
 
 function preload(){
   palettesData = loadJSON("data/palettes.json");
@@ -85,8 +94,10 @@ function setup(){
   createCanvas(canvasW, canvasH);
   selectPalette(5, true, false);
   background(0, 0, 0);
+  // setInterval( ()=> { draw() }, 100 );
 }
 
+  // requestAnimationFrame(draw);
 
 function draw() {
   //  count++;
@@ -95,28 +106,51 @@ function draw() {
   //   background(0, 0, 0, 5);
   // }
 
-  if(bubbleArray.length>bubbleQuantity){
-    bubbleArray.splice(0, 1);
+
+  if(atomArray.length>atom){
+    atomArray.splice(0, 1);
   }
-  else if(bubbleArray.length<bubbleQuantity){
-    bubbleArray.push(
-      new Bubble(random(0, width), random(0, height), random(bubble.minSize, bubble.maxSize)))
+  else if(atomArray.length<atomQuantity){
+    atomArray.push(
+      new Atom(random(0, width), random(0, height), atom))
   }
 
-  for(i = 0; i < bubbleArray.length; i++ ){
-    bubbleArray[i].move();
-    bubbleArray[i].show();
+  for(i = 0; i < atomArray.length; i++ ){
+    atomArray[i].resize()
+    atomArray[i].move();
+    atomArray[i].show();
   }
 }
 
-function mouseClicked() {
-  bubbleQuantity++;
-  bubbleArray.push(
-    new Bubble(mouseX, mouseY, random(bubble.minSize, bubble.maxSize)))
+let mouseX1;
+let mouseY1;
+let mouseX2;
+let mouseY2;
+
+function mousePressed() {
+  mouseX1 = mouseX;
+  mouseY1 = mouseY;
+}
+function mouseReleased() {
+  mouseX2 = mouseX;
+  mouseY2 = mouseY;
+  calcDirection()
+  atomQuantity++;
+  atomArray.push(
+    new Atom(mouseX2, mouseY2, atom))
 }
 
-function mouseDragged() {
-  bubbleQuantity++;
-  bubbleArray.push(
-    new Bubble(mouseX, mouseY, random(bubble.minSize, bubble.maxSize)))
+function calcDirection() {
+  let x = mouseX2-mouseX1;
+  let y = mouseY2-mouseY1;
+  atom.movHor = x/windowWidth;
+  atom.movVer = y/windowWidth;
+  //map(col2, 0, 255, 0, 100)
 }
+
+
+// function mouseDragged() {
+//   atomQuantity++;
+//   atomArray.push(
+//     new Atom(mouseX, mouseY, atom))
+// }
--- js/painter/main_bubble.js
+++ js/painter/main_bubble.js
@@ -1,30 +1,38 @@
-class Bubble {
-  constructor(_x, _y, _radius) {
+class Atom {
+  constructor(_x, _y, _atom) {
     //TODO apvienot mainīgos objektā
-    const {alphaMin, alphaMax} = bubble;
-    //TODO cleanUp variables!!!
+    // const {alphaMin, alphaMax} = bubble;
     this.x = _x;
     this.y = _y;
-    this.radius = _radius;
-    // this.diameter = this.radius*2;
-    // this.roundness = bubble.roundness;
-    
-    this.alphaMin = bubble.alphaMin;
-    this.alphaMax = bubble.alphaMax;
+
+    this.sizeMin = _atom.sizeMin;
+    this.sizeMax = _atom.sizeMax;
+    this.sizeRand = _atom.sizeRand;
+    this.size = random(_atom.sizeMin, _atom.sizeMax);
+
+
+    this.movHor = _atom.movHor;
+    this.movVer = _atom.movVer;
+    this.movTremble = _atom.movTremble;
+    this.movSpeed = _atom.movSpeed;
+    this.movRandX = _atom.movRandX;
+    this.movRandY = _atom.movRandY;
+    this.movRandZ = _atom.movRandZ;
 
     // this.strokeWeightMin;
     // this.strokeWeightMax;
 
     //MOVEMENT
-    this.xoff1 = random(1000);
-    this.xoff2 = random(1000)
-    this.xoff3 = random(1000)
+    this.xoffX = random(1000);
+    this.xoffY = random(1000);
+    this.xoffZ = random(1000);
+    this.xoffSize = random(1000);
+    this.xoffCol = random(1000);
+
     //NB Between 1(random) and 0(flat) 
     this.incrementVerySlow = random(0.0001, 0.0001)
     this.incrementSlow = random(0.001, 0.005)
-    this.incrementNormal = random(0.01, 0.005)
-    this.move1 = random(0.2);
-    this.move2 = random(-0.1, 0.1);
+    this.incrementNormal = random(0.01, 0.05)
 
     //OBJECT TYPE
     //TODO solve how to apply in different situations
@@ -40,9 +48,11 @@ class Bubble {
     this.h_shade = this.h;
     this.s_shade = this.s;
     this.l_shade = this.l;
+    this.alphaMin = _atom.alphaMin;
+    this.alphaMax = _atom.alphaMax;
     this.a = random(this.alphaMin, this.alphaMax);
-
   }
+
   selectType(p1, p2, pTotal){
     const probability = random(pTotal);
     let variation = null;
@@ -52,17 +62,26 @@ class Bubble {
     return variation;
   }
   //====MOVE===================
+
+  resize(){
+    this.xoffSize += this.sizeRand;
+    this.size = this.changeRadius(this.type, this.sizeMin, this.sizeMax, this.xoffSize);
+  }
+
   move(){
-    this.xoff1 += this.incrementSlow;
-    this.xoff2 += this.incrementNormal;
-    this.xoff3 += this.incrementNormal;
-    this.radius = this.changeRadius(this.type, bubble.minSize, bubble.maxSize, this.xoff3);
-    this.x = this.moveDirection(this.x, this.radius, this.move1)
-    this.y = this.moveDirection(this.y, this.radius, 0.1);
-    this.x = this.moveTremble(this.x, this.radius, this.move1);
-    this.y = this.moveTremble(this.y, this.radius, this.move1);
-    this.x = this.movePerlin(this.x, this.radius, -0.5, 0.5, this.xoff1);
-    this.y = this.movePerlin(this.y, this.radius, -0.5, 0.5, this.xoff2);
+    this.xoffX += this.movRandX;
+    this.xoffY += this.movRandY;
+    this.xoffZ += this.movRandZ;
+
+    this.x = this.moveDirection(this.x, this.size, this.movHor)
+    this.y = this.moveDirection(this.y, this.size, this.movVer);
+    // this.x = this.moveTremble(this.x, this.size, this.movTremble);
+    // this.y = this.moveTremble(this.y, this.size, this.movTremble);
+    this.x = this.movePerlin(this.x, this.size, -this.movSpeed/5, this.movSpeed/5, this.xoffZ);
+    this.y = this.movePerlin(this.y, this.size, -this.movSpeed/5, this.movSpeed/5, this.xoffZ);
+    this.x = this.movePerlin(this.x, this.size, -this.movSpeed, this.movSpeed, this.xoffX);
+    this.y = this.movePerlin(this.y, this.size, -this.movSpeed, this.movSpeed, this.xoffY);
+
     this.edgeless();
   }
 
@@ -111,27 +130,27 @@ class Bubble {
     noStroke()
     // strokeWeight(0.5);
     // noFill();
-    this.colorShadesNoise();
+    // this.colorShadesNoise();
     // this.alphaShades();
     // this.colorImage();
     // this.h_shade++;
     fill(this.h, this.s_shade, this.l_shade)
 
     // var radius_local = map(this.a, 30, 100, 0, 3)
-    // this.radius = (this.radius+radius_local)/2
-    // this.radius = radius_local;
-    ellipse(this.x, this.y, this.radius, this.radius);
+    // this.size = (this.size+radius_local)/2
+    // this.size = radius_local;
+    ellipse(this.x, this.y, this.size, this.size);
   }
 
 
   // alphaShades(){
-  //   this.h = map(noise(this.xoff3), 0, 1, bubble.alphaMin, bubble.alphaMax);
+  //   this.h = map(noise(this.xoffZ), 0, 1, bubble.alphaMin, bubble.alphaMax);
   // }
 
   colorShadesNoise(){
-    this.h_shade = map(noise(this.xoff1), 0, 1, this.h-20, this.h+20);
-    this.s_shade = map(noise(this.xoff2), 0, 1, this.s-10, this.s+10);
-    this.l_shade = map(noise(this.xoff3), 0, 1, this.l-10, this.l+10);
+    this.h_shade = map(noise(this.xoffX), 0, 1, this.h-20, this.h+20);
+    this.s_shade = map(noise(this.xoffY), 0, 1, this.s-10, this.s+10);
+    this.l_shade = map(noise(this.xoffZ), 0, 1, this.l-10, this.l+10);
     
     if (this.h_shade > 360) this.h_shade = 0;
     if (this.h_shade < 0) this.h_shade = 360;
