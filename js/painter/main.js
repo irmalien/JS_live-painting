39ea7346bdcee215b76f85743aeb2c868183b496
irmalien <mikus.vanags@gmail.com>
2019-02-26 10:04:36 +0100

    before next version

--- js/painter/main.js
+++ js/painter/main.js
@@ -1,100 +1,172 @@
-// let img;
 const windowWidth = document.documentElement.clientWidth;
 const windowHeight = document.documentElement.clientHeight;
 
-function preload(){
-  // img - loadImage('kitten.jpg')
-}
-let d =8
-let n =5
-let c =0.01;
-let k = 5/8;
-let sliderD;
-let sliderN;
-let sliderC;
-let sliderSize;
-let angle = 0;
+const canvasW = windowWidth  //5606 //1000 //4400; //windowWidth //2200
+const canvasH = windowHeight   //3737 //667 //2616; //windowHeight //1308
+
 let count = 0;
-let canv;
+let atomArray = [];
+let atomQuantity = 0;
+let colorsData; //imported json data
 
-function setup() {
-  canv = createCanvas(windowWidth, windowHeight);
-  // canv = createCanvas(1280, 720);
-  // angleMode(DEGREES)
-  w1=width
-  h1=height
-  sliderD=createSlider(1, 30, 15, 0.01) //6 //14
-  sliderN=createSlider(1, 30, 30, 0.01) //17 //15
-  sliderC=createSlider(1, 100, 4, 0.01)
-  sliderSize=createSlider(10, 1000, 900, 1)
-  frameRate(3)
-  // z = TWO_PI-0.1
+// const atomQuantity2 ={
+//   value: 10,
+
+//   set( newValue, para ) { return; this.value = newValue },
+//   get()           { return this.value; }
+// },
+
+const atom = {
+
+  //OBJECT
+  //lifetime: 0 = infinity?
+  lifespan: 100,
+
+  sizeMin: 3,
+  sizeMax: 3,
+  sizeRand: 0.05,
+
+  // roundness: 1,
+  // rotationMin: 0,
+  // rotationMax: 360,
+
+  // minStroke: 1, 
+  // maxStroke: 10, 
+
+
+
+  //COLORS
+  colArray: [],
+  colAlphaMin: 0.9,
+  colAlphaMax: 1.0,
+
+  //DYNAMICS (relative to atom radius)
+  movVer: 0.5,
+  movHor: 0.1,
+  movTremble: 0.1,
+  movSpeed: 0.5,
+  movRandX: 0.0,
+  movRandY: 0.5,
+  movRandZ: 0.9,
+};
+
+
+// function relativeSize(){
+//   const relativeSize = canvasW/1000;
+//   atom.minSize = atom.minSize * relativeSize;
+//   atom.maxSize = atom.maxSize * relativeSize;
+//   atom.movement = atom.movement * relativeSize;
+// }
+
+function preload(){
+  colorsData = loadJSON("data/palettes.json");
+  img = loadImage('img/gustavs.jpg');
 
 }
 
-let angleStep = 0.000000
-let trail = 200; //45
-let trueCount = 0;
-let middle = false;
+function selectPalette(size, white, black){
+  let selectRand = floor(random(0, colorsData.palettes.length))
 
-function draw() {
-  count++
-  trueCount++
-  if (trueCount >= 1500){
-    middle = true;
+  if (size > colorsData.palettes[selectRand].length){
+    size = colorsData.palettes[selectRand].length
   };
 
-  if(count >= 18){
-    count=0;
-    if (middle == false){
-      trail++;
-    }
-    else{
-      trail--
-    }
-  }
-  console.log(count, trueCount, trail, middle)
-  console.log(d, n, c)
-  console.log(frameRate())
-  background(0)
-  d=sliderD.value();
-  n=sliderN.value();
-  step=0.002;
-  c=c+step;
-  // c=sliderC.value();
-  angleStep=angleStep-0.000000001;
-
-  size=sliderSize.value();
-  k= n/d;
-  // w1++
-  // h1++
-  // z = z-0.1
-  // r=r+0.1;
-  let newC = c
-  let alfa = 0;
-  angle = angle+angleStep;
-  translate(w1/2, h1/2);
-  noFill()
-  for(let i=250; i>=250-trail; i--){
-    let newStep = i*(step/1000);
-    newC = newC+newStep;
-    alfa = alfa+0.5
-    rotate(angle);
-    beginShape();
-    for (let a=0+TWO_PI/newC; a<TWO_PI*d; a+=TWO_PI/newC){
-      const r= size*cos(k*a)
-      const x = r*cos(a);
-      const y = r*sin(a);
-      stroke(40,240,250, alfa);
-      strokeWeight(0.25);
-      vertex(x,y);
-    }
-    endShape();
-    // saveCanvas('rose'+count,'png')
+  for (let i=0; i<size; i++){
+    let hslNew = hexToHSL(colorsData.palettes[selectRand][i])
+    atom.colArray.push(hslNew);
   }
 
+  if (white){
+    let colorTemp = [0, 0, 100];
+    atom.colArray.push(colorTemp);
+  };
 
-  
-  // image(img, 0, 0, mouseX, mouseY)
-  // ellipse(0, 0, 100, 100)
+  if (black){
+    let colorTemp = [0, 0, 0];
+    atom.colArray.push(colorTemp);
+  };
 }
+
+function setup(){
+  colorMode(HSL, 360, 100, 100, 100);
+  createCanvas(canvasW, canvasH);
+  selectPalette(5, true, false);
+  background(0, 0, 0);
+  // setInterval( ()=> { draw() }, 100 );
+}
+
+  // requestAnimationFrame(draw);
+
+function draw() {
+  // translate(width/2, height/2);
+  //  count++;
+  // if (count == 10){
+  //   count =0;
+  //   background(0, 0, 0, 5);
+  // }
+
+
+  const tester = new Atom(random(0, width), random(0, height), atom);
+  tester.type = B;
+
+  if(atomArray.length>atom){
+    atomArray.splice(0, 1);
+  }
+  else if(atomArray.length<atomQuantity){
+    atomArray.push(
+      new Atom(random(0, width), random(0, height), atom))
+  }
+  for(i = atomArray.length-1; i >= 0; i-- ){
+    if (atomArray[i].count(i) == false){
+      // atomArray[i] = new Atom(random(0, width), random(0, height), atom);
+      atomArray.splice(i, 1);
+    }
+    else {
+      atomArray[i].resize();
+      atomArray[i].move();
+    }
+
+
+    // atomArray[i].show();
+  }
+}
+
+
+
+let mouseX1;
+let mouseY1;
+let mouseX2;
+let mouseY2;
+
+function mousePressed() {
+  mouseX1 = mouseX;
+  mouseY1 = mouseY;
+}
+function mouseReleased() {
+  mouseX2 = mouseX;
+  mouseY2 = mouseY;
+  calcDirection()
+  atomQuantity++;
+  atomArray.push(
+    new Atom(mouseX2, mouseY2, atom))
+}
+
+function calcDirection() {
+  let x = mouseX2-mouseX1;
+  let y = mouseY2-mouseY1;
+  atom.movHor = x/windowWidth;
+  atom.movVer = y/windowWidth;
+  //map(col2, 0, 255, 0, 100)
+}
+
+
+
+function selectColor(){
+
+}
+
+// function mouseDragged() {
+//   atomQuantity++;
+//   atomArray.push(
+//     new Atom(mouseX, mouseY, atom))
+// }
